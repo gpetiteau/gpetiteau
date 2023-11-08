0cff8c7e6faefe88b8154cf3971b04eaea3ecc9e
gpetiteau <146841967+gpetiteau@users.noreply.github.com>
2023-11-08 10:16:06 +0100

    Delete rectangle.php

--- rectangle.php
+++
@@ -1,9 +0,0 @@
-<?php
-$largeur = readline("Saisire la largeur du rectangle : ");
-$hauteur = readline("Saisire la hauteur du rectangle : ");
-for($i=0;$i<$hauteur;$i=$i+1){
-    for($j=0;$j<$largeur;$j=$j+1){
-   echo"*".' ';
-}
-    echo PHP_EOL;
-}
